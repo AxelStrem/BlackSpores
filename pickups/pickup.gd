5f929b8a775af54fd0ccd5dcf43f0d20a4fc48ed
ddmitriev <ddmitriev@intermedia.net>
2024-01-20 14:32:53 +0300

    add pickup movement and color

--- pickups/pickup.gd
+++ pickups/pickup.gd
@@ -1,5 +1,5 @@
 extends Node3D
-
+var time=0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -7,9 +7,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotate_y(0.1)
-	
+	rotate_y(delta*3)
+	time+=delta
+	$MeshInstance3D.position.y = sin(time*5)*0.2
 
 
 func _on_area_3d_body_entered(body):
+	queue_free()
+
+
+func _on_area_3d_area_entered(area):
 	queue_free()
