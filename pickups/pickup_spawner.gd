e882ad6cf70289fde06cf66f218d9df4ff8f59c0
Dmitrii Khrustalev <axelstrem@gmail.com>
2024-01-21 18:05:09 +0400

    added more pickups

--- pickups/pickup_spawner.gd
+++ pickups/pickup_spawner.gd
@@ -2,7 +2,11 @@ extends Node3D
 
 @export var spawn_probability = 0.2
 
-var pickups = [preload("res://pickups/datapoint.tscn"), preload("res://pickups/speedboost.tscn"), preload("res://pickups/antigrav_boots.tscn")]
+var pickups = [preload("res://pickups/datapoint.tscn"),
+ preload("res://pickups/speedboost.tscn"),
+ preload("res://pickups/antigrav_boots.tscn"),
+ preload("res://pickups/teleporter.tscn"),
+ preload("res://pickups/ward.tscn")]
 
 func _ready():
 	if randf()<=spawn_probability:
